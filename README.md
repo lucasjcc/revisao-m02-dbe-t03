a3f236388f6d53d37d90da4fecaceb1bc14841a4
Lucas Carvalho <lucasjorge.cap@gmail.com>
2023-08-14 16:45:45 -0300

    primeiro commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# API PARA CONTROLE DE AULAS EM UM CURSO À DISTÂNCIA
+
+## Contexto
+Você foi contratado pela maior faculdade à distância da América Latina para construir uma API para controle de aula dos alunos.
+
+Essa API fará parte de uma aplicação web que permitirá aos alunos fazerem o controle das aulas que eles assistiram ou ainda está pendente
+
+## Projeto
+Sua API deve permitir
+- Somente professores:
+  - [ ] Listar as contas
+  - [ ] Criar uma conta
+  - [ ] Editar uma conta
+  - [ ] Deletar uma conta (mediante à verificação de senha)
+- Alunos:
+  - [ ] Criar uma aula
+  - [ ] Listar aulas
+  - [ ] Detalhar uma aula
+  - [ ] Deletar uma aula (mediante à verificação de senha)
+  - [ ] Filtrar as aulas por concluídas
+
+## Requisição
+Em toda requisição, o(s) dado(s) para reconhecer o usuário logado deve ser enviado(s).
---
+++ dados.js
@@ -0,0 +1,104 @@
+const dados = [
+    {
+        "id": 1,
+        "nome": "Caldeira",
+        "email": "caldeira@gmail.com",
+        "cargo": "professor"
+	},
+	{
+        "id": 2,
+        "nome": "Ivor Q. Pruitt",
+        "email": "tellus@outlook.org",
+        "cargo": "aluno",
+        "aulas": [
+            {
+                "id": 1,
+                "nome": "Cursus Luctus Corporation",
+                "vista": false
+            },
+            {
+                "id": 2,
+                "nome": "Mi Eleifend Corporation",
+                "vista": false
+            },
+            {
+                "id": 3,
+                "nome": "Lacus Ut Nec Industries",
+                "vista": false
+            }
+        ]
+	},
+	{
+		"id": 3,
+		"nome": "Hunter H. Bonner",
+		"email": "odio.auctor.vitae@google.ca",
+        "senha": "123",
+        "cargo": "aluno",
+        "aulas": [
+            {
+                "id": 4,
+                "nome": "Turpis Aliquam Ltd",
+                "vista": false
+            },
+            {
+                "id": 5,
+                "nome": "Amet Foundation",
+                "vista": true
+            },
+            {
+                "id": 6,
+                "nome": "Aliquet Diam Inc.",
+                "vista": false
+            },
+            {
+                "id": 7,
+                "nome": "Feugiat Institute",
+                "vista": true
+            },
+            {
+                "id": 8,
+                "nome": "Ameb Risus Quis Ltd",
+                "vista": true
+            },
+            {
+                "id": 9,
+                "nome": "Odio Aliquam Vulputate Industries",
+                "vista": false
+            },
+            {
+                "id": 10,
+                "nome": "Lectus Rutrum Urna LLP",
+                "vista": true
+            },
+            {
+                "id": 11,
+                "nome": "Ameo Curabitur Company",
+                "vista": true
+            },
+            {
+                "id": 12,
+                "nome": "Adipiscing Fringilla Limited",
+                "vista": true
+            },
+            {
+                "id": 13,
+                "nome": "Nunc Ac Sem PC",
+                "vista": true
+            },
+            {
+                "id": 14,
+                "nome": "Amet Aliquam Inc.",
+                "vista": false
+            },
+            {
+                "id": 15,
+                "nome": "Proin Nisl Limited",
+                "vista": true
+            }
+        ]
+	}
+]
+
+module.exports = {
+    dados
+}
